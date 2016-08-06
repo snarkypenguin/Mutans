01136e452e6483719783d14c5e24402257c61f8e
Randall Gray <Randall.Gray@grayrabble.org>
2016-08-06 10:41:49 +1000

    forgot plant.scm ...the ^^ commit will not run because it already knows about plant.scm

---
+++ Example-Model/model/plant.scm
@@ -0,0 +1,28 @@
+; -*- mode: scheme; -*-
+;-  Identification and Changes
+
+;--
+;	plant.scm -- Written by Randall Gray 
+;	Initial coding: 
+;		Date: 2016.08.05
+;		Location: zero:/home/randall/Thesis/Example-Model/model/plant.scm
+;
+;	History:
+;
+
+;-  Code 
+
+(load "plant-classes.scm")
+(load "plant-declarations.scm")
+(load "plant-methods.scm")
+
+;-  The End 
+
+
+;;; Local Variables: 
+;;; comment-end: "-;" ;;;
+;;; comment-start: ";;; " ;;;
+;;; mode: scheme ;;;
+;;; outline-regexp: ";-+" ;;;
+;;; comment-column: 0 ;;;
+;;; End:
