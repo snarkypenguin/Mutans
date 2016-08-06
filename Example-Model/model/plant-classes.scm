e11708dd0879dcc9b9ebe937a8a998cdce6f0dda
Randall Gray <Randall.Gray@grayrabble.org>
2016-08-06 10:40:08 +1000

    Adding i-b plant class

---
+++ Example-Model/model/plant-classes.scm
@@ -0,0 +1,49 @@
+; -*- mode: scheme; -*-
+;-  Identification and Changes
+
+;--
+;	plant-classes.scm -- Written by Randall Gray 
+;	Initial coding: 
+;		Date: 2016.08.02
+;		Location: zero:/home/randall/Thesis/Example-Model/model/plant-classes.scm
+;
+;	History:
+;
+
+;-  Code 
+
+(define <simple-plant>
+  (make-class (inherits-from <model-maintenance> <thing>)
+				  (state-variables
+					max-age ;; dies beyond this
+					max-mass ;; unlikely to reach this
+					age ;; age of plant in [0,4)
+					lai ;; leaf area index in (0,10)
+					water-stress  ;; [0,1]
+					water-needs ;; amount of water required for a square metre
+					water-stress-effect ;; True if we factor water stress
+											  ;; into fruiting
+					reproduction-period ;; how long between reproductions
+					reproduction-offset ;; 
+					reproduction-mechanism ;; <fruit> or (list <agent> method val)
+					fruiting-rate      ;; relative to mass, influenced by
+											 ;; level of water stress
+					seeds-per-fruit
+					environ            ;; #f or a landscape thing
+					;; <thing> mass is our mass
+					)))
+(register-class <simple-plant>)
+
+
+
+
+;-  The End 
+
+
+;;; Local Variables: 
+;;; comment-end: "-;" ;;;
+;;; comment-start: ";;; " ;;;
+;;; mode: scheme ;;;
+;;; outline-regexp: ";-+" ;;;
+;;; comment-column: 0 ;;;
+;;; End:
---
+++ Example-Model/model/plant-declarations.scm
@@ -0,0 +1,27 @@
+; -*- mode: scheme; -*-
+;-  Identification and Changes
+
+;--
+;	plant-declarations.scm -- Written by Randall Gray 
+;	Initial coding: 
+;		Date: 2016.08.05
+;		Location: zero:/home/randall/Thesis/Example-Model/model/plant-declarations.scm
+;
+;	History:
+
+;-  Code 
+
+;; Individual-based plants
+(declare-method fruit-count "returns the number of fruit produced by an agent")
+
+		
+;-  The End 
+
+
+;;; Local Variables: 
+;;; comment-end: "-;" ;;;
+;;; comment-start: ";;; " ;;;
+;;; mode: scheme ;;;
+;;; outline-regexp: ";-+" ;;;
+;;; comment-column: 0 ;;;
+;;; End:
