082a72b7b981b307a968586a0e14a935f7a8b1b3
Randall Gray <Randall.Gray@grayrabble.org>
2016-11-13 08:12:24 +1100

    and the others...

---
+++ Example-Model/model/run-model.scm
@@ -0,0 +1,47 @@
+; -*- mode: scheme; -*-
+;-  Identification and Changes
+
+;--
+;	run-model.scm -- Written by Randall Gray 
+;	Initial coding: 
+;		Date: 2016.11.09
+;		Location: zero.grayrabble.org:/home/randall/Thesis/Example-Model/model/run-model.scm
+;
+;	History:
+;
+
+;-  Copyright 
+
+;
+;   (C) 2016 Randall Gray
+;   All rights reserved
+;
+
+;-  Discussion 
+
+;-  Configuration stuff 
+
+;-  Included files 
+
+;-  Variables/constants both public and static
+
+;--    Static data
+
+;--    Public data 
+
+;-  Code 
+
+(load "model.config")
+(Doit Q)
+
+
+;-  The End 
+
+
+;;; Local Variables: 
+;;; comment-end: " ;;;" ;;;
+;;; comment-start: ";;; " ;;;
+;;; mode: scheme ;;;
+;;; outline-regexp: ";-+" ;;;
+;;; comment-column: 0 ;;;
+;;; End:
